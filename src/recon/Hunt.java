a1115f2cbfcf99f4fcce485b7df04839bee72512
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-09-02 18:16:08 +0200

    updated new feature: possible service detection
    - possible service, note added to output file
    - refactored "possibleServices" method

--- src/recon/Hunt.java
+++ src/recon/Hunt.java
@@ -10,27 +10,30 @@ import java.net.SocketException;
 
 public class Hunt {
 	protected String ipAddress;
+	private static String possibleService;
 	
 	public Hunt(String ipAddress) {
 		this.ipAddress = ipAddress;
 	}
 	
-	public static void possibleServices(int port) {
+	public static String possibleServices(int port) {
 		if(port == 20 || port == 21) {
-			System.out.printf("\t\tport %d, possible FTP service detected!\n", port);
+			possibleService = String.format("\t\tport %d, possible FTP service detected!\n", port);
 		} else if(port == 22) {
-			System.out.println("\t\tport 22, possible SSH service detected!");
+			possibleService = "\t\tport 22, possible SSH service detected!";
 		} else if(port == 23) {
-			System.out.println("\t\tport 23, possible Telnet service detected!");
+			possibleService = "\t\tport 23, possible Telnet service detected!";
 		} else if(port == 25 || port == 456 || port == 587) {
-			System.out.printf("\t\tport %d, possible SMTP service detected!", port);
+			possibleService = String.format("\t\tport %d, possible SMTP service detected!", port);
 		} else if(port == 69) {
-			System.out.println("\t\tport 69, possible TFTP service detected!");
+			possibleService = "\t\tport 69, possible TFTP service detected!";
 		} else if(port == 137 || port == 139 || port == 445) {
-			System.out.printf("\t\tport %d, possible SMB service detected!", port);
+			possibleService = String.format("\t\tport %d, possible SMB service detected!", port);
 		} else if(port == 80 || port == 443 || port == 8080) {
-			System.out.printf("\t\tport %d, possible HTTP/HTTPS service detected!", port);
+			possibleService = String.format("\t\tport %d, possible HTTP/HTTPS service detected!", port);
 		}
+		
+		return possibleService;
 	}
 	
 	public void scanPorts(int minPort, int maxPort, int lastOctet) {
@@ -44,10 +47,10 @@ public class Hunt {
 					socket.close();
 					
 					System.out.printf("\t%s.%d, port %d, status: open\n", this.ipAddress, lastOctet, port);
-					
-					possibleServices(port);
+					System.out.println(possibleServices(port));
 					
 					writer.write(String.format("\t%s.%d, port %d, status: open\n", this.ipAddress, lastOctet, port));
+					writer.write(possibleServices(port));
 					writer.close();
 				} catch(ConnectException exc) {
 					System.out.printf("\t%s.%d, port %d, status: closed\n", this.ipAddress, lastOctet, port);
@@ -60,4 +63,3 @@ public class Hunt {
 		}
 	}
 }
-
