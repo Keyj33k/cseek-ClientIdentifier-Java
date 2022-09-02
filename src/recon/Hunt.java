8fdddc026643f0bc26af8618d18f91508d5b031d
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-09-02 17:47:07 +0200

    added feature: detect possible service via port num

--- src/recon/Hunt.java
+++ src/recon/Hunt.java
@@ -15,6 +15,24 @@ public class Hunt {
 		this.ipAddress = ipAddress;
 	}
 	
+	public static void possibleServices(int port) {
+		if(port == 20 || port == 21) {
+			System.out.printf("\t\tport %d, possible FTP service detected!\n", port);
+		} else if(port == 22) {
+			System.out.println("\t\tport 22, possible SSH service detected!");
+		} else if(port == 23) {
+			System.out.println("\t\tport 23, possible Telnet service detected!");
+		} else if(port == 25 || port == 456 || port == 587) {
+			System.out.printf("\t\tport %d, possible SMTP service detected!", port);
+		} else if(port == 69) {
+			System.out.println("\t\tport 69, possible TFTP service detected!");
+		} else if(port == 137 || port == 139 || port == 445) {
+			System.out.printf("\t\tport %d, possible SMB service detected!", port);
+		} else if(port == 80 || port == 443 || port == 8080) {
+			System.out.printf("\t\tport %d, possible HTTP/HTTPS service detected!", port);
+		}
+	}
+	
 	public void scanPorts(int minPort, int maxPort, int lastOctet) {
 		try {
 			for(int port = minPort; port < maxPort + 1; port++) {
@@ -26,6 +44,8 @@ public class Hunt {
 					socket.close();
 					
 					System.out.printf("\t%s.%d, port %d, status: open\n", this.ipAddress, lastOctet, port);
+					
+					possibleServices(port);
 					
 					writer.write(String.format("\t%s.%d, port %d, status: open\n", this.ipAddress, lastOctet, port));
 					writer.close();
