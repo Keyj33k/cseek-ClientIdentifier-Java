66962947df3375700a324ce87c62d5a631e3dda6
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-09-06 15:53:51 +0200

    added scan result counter

--- src/main/java/recon/Hunt.java
+++ src/main/java/recon/Hunt.java
@@ -9,11 +9,12 @@ import java.net.ConnectException;
 import java.net.SocketException;
 
 abstract class Hunt {
-	protected String ipAddress;
+	public String ipAddress;
 	private static String possibleService;
+	private static int portStatusCount = 0;
 	
 	public Hunt(String ipAddress) {
-		this.ipAddress = ipAddress;
+	 	this.ipAddress = ipAddress;
 	}
 	
 	private static String possibleServices(int port) {
@@ -37,6 +38,8 @@ abstract class Hunt {
 	}
 	
 	public void scanPorts(int minPort, int maxPort, int lastOctet) throws IOException {
+		int hostsToScan = maxPort - minPort + 1;
+		
 		for(int port = minPort; port < maxPort + 1; port++) {
 			BufferedWriter writer = new BufferedWriter(new FileWriter("output/nethuntResults.txt", true));
 			Socket socket = new Socket();
@@ -45,18 +48,24 @@ abstract class Hunt {
 				socket.connect(new InetSocketAddress(this.ipAddress, port));
 					
 				System.out.printf("\t%s.%d, port %d, status: open\n", this.ipAddress, lastOctet, port);
-				System.out.println(possibleServices(port));
+				possibleServices(port);
 					
 				writer.write(String.format("\t%s.%d, port %d, status: open\n", this.ipAddress, lastOctet, port));
 				writer.write(possibleServices(port) + "\n");
 			} catch(ConnectException exc) {
-				System.out.printf("\t%s.%d, port %d, status: closed\n", this.ipAddress, lastOctet, port);
+				portStatusCount += 1;
 			} catch(SocketException exc) {
-				System.out.printf("\t%s.%d, port %d, status: closed\n", this.ipAddress, lastOctet, port);
+				portStatusCount += 1;
 			} finally {
 				socket.close();
 				writer.close();
 			}
+		}
+		
+		if(hostsToScan == portStatusCount) {
+			System.out.printf("\tport scan done => %d ports scanned, all scanned ports are closed\n", hostsToScan);
+		} else {
+			System.out.printf("\tport scan done => %d ports scanned, %d ports are closed\n", hostsToScan, portStatusCount);
 		}
 	}
 }
